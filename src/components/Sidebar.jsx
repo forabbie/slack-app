3d7fe211e38cb41ed707afcfc1415060619f4573
iannnna <julianna.barrera03@gmail.com>
2023-05-30 20:55:59 +0800

    test: SideMenu dropdowm

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -25,7 +25,149 @@ const Sidbar = () => {
           </button>
         </div>
         <div className="border-b border-white/25"></div>
-        <ul className="menu py-3">
+                    <nav aria-label="Main Nav" className="text-white px-4 mt-6 flex flex-col space-y-1">
+                        <details className="group [&_summary::-webkit-details-marker]:hidden">
+                            <summary className="flex cursor-pointer items-center justify-between rounded-lg px-4 py-2 hover:bg-gray-400/[0.4]" >
+                                <div className="flex items-center gap-2">
+                                    <svg
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        className="h-5 w-5 opacity-75"
+                                        fill="none"
+                                        viewBox="0 0 24 24"
+                                        stroke="currentColor"
+                                        strokeWidth="2"
+                                    >
+                                        <path
+                                            strokeLinecap="round"
+                                            strokeLinejoin="round"
+                                            d="M17 20h5v-2a3 3 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20H2v-2a3 3 0 015.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857m0 0a5.002 5.002 0 019.288 0M15 7a3 3 0 11-6 0 3 3 0 016 0zm6 3a2 2 0 11-4 0 2 2 0 014 0zM7 10a2 2 0 11-4 0 2 2 0 014 0z"
+                                        />
+                                    </svg>
+
+                                    <span className="text-sm font-medium"> Channels </span>
+                                </div>
+
+                                <span className="shrink-0 transition duration-300 group-open:-rotate-180">
+                                    <svg
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        className="h-5 w-5"
+                                        viewBox="0 0 20 20"
+                                        fill="currentColor"
+                                    >
+                                        <path
+                                            fillRule="evenodd"
+                                            d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                                            clipRule="evenodd"
+                                        />
+                                    </svg>
+                                </span>
+                            </summary>
+
+                            <nav aria-label="Channels Nav" className="mt-2 flex flex-col px-4">
+                                <a href="#" className="flex items-center gap-2 rounded-lg px-4 py-2 hover:bg-gray-400/[0.4]">
+                                  <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    fill="none"
+                                    viewBox="0 0 24 24"
+                                    stroke="currentColor"
+                                    className="w-5 h-4"
+                                  >
+                                    <path
+                                      strokeLinecap="round"
+                                      strokeLinejoin="round"
+                                      d="M16.5 10.5V6.75a4.5 4.5 0 10-9 0v3.75m-.75 11.25h10.5a2.25 2.25 0 002.25-2.25v-6.75a2.25 2.25 0 00-2.25-2.25H6.75a2.25 2.25 0 00-2.25 2.25v6.75a2.25 2.25 0 002.25 2.25z"
+                                    />
+                                  </svg>
+                                  <span className="text-sm font-medium"> Item 1 </span>
+                                </a>
+                                <a href="#" className="flex items-center gap-2 rounded-lg px-4 py-2 hover:bg-gray-400/[0.4]" >
+                                  <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    fill="none"
+                                    viewBox="0 0 24 24"
+                                    strokeWidth="1.5"
+                                    stroke="currentColor"
+                                    className="w-5 h-5 bg-white/25 rounded"
+                                  >
+                                    <path
+                                      strokeLinecap="round"
+                                      strokeLinejoin="round"
+                                      d="M12 6v12m6-6H6"
+                                    />
+                                  </svg>
+                                    <span className="text-sm font-medium">Add Channel </span>
+                                </a>
+                            </nav>
+
+                            
+                        </details>
+
+                        <details className="group [&_summary::-webkit-details-marker]:hidden">
+                            <summary className="flex cursor-pointer items-center justify-between rounded-lg px-4 py-2 hover:bg-gray-400/[0.4]" >
+                                <div className="flex items-center gap-2">
+                                    <svg
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        className="h-5 w-5 opacity-75"
+                                        fill="none"
+                                        viewBox="0 0 24 24"
+                                        stroke="currentColor"
+                                        strokeWidth="2"
+                                    >
+                                        <path
+                                            strokeLinecap="round"
+                                            strokeLinejoin="round"
+                                            d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"
+                                        />
+                                    </svg>
+
+                                    <span className="text-sm font-medium"> Direct Messages </span>
+                                </div>
+
+                                <span className="shrink-0 transition duration-300 group-open:-rotate-180">
+                                    <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    className="h-5 w-5"
+                                    viewBox="0 0 20 20"
+                                    fill="currentColor"
+                                    >
+                                    <path
+                                        fillRule="evenodd"
+                                        d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                                        clipRule="evenodd"
+                                    />
+                                    </svg>
+                                </span>
+                            </summary>
+
+                            <nav aria-label="Account Nav" className="mt-2 flex flex-col px-4">
+                                <a href="#" className="flex items-center gap-2 rounded-lg px-4 py-2 hover:bg-gray-400/[0.4]" >
+                                  <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    fill="none"
+                                    viewBox="0 0 24 24"
+                                    strokeWidth="1.5"
+                                    stroke="currentColor"
+                                    className="w-5 h-5 bg-white/25 rounded"
+                                  >
+                                    <path
+                                      strokeLinecap="round"
+                                      strokeLinejoin="round"
+                                      d="M12 6v12m6-6H6"
+                                    />
+                                  </svg>
+                                    <span className="text-sm font-medium">Open a Direct Message </span>
+                                </a>
+                                <div className='directMessageList'>
+                                            <a  href='#' className="flex items-center gap-2 rounded-lg px-4 py-2 hover:bg-gray-400/[0.4]">
+                                                <span className="text-sm font-medium">Ianna</span>
+                                            </a>
+                                </div>
+                            </nav>
+                        </details>
+                    </nav>
+
+        
+        {/* <ul className="menu py-3">
           <li className="menu-title">
             <a>Channels</a>
           </li>
@@ -75,7 +217,8 @@ const Sidbar = () => {
           <li>
             <a>Item 2</a>
           </li>
-        </ul>
+        </ul> */}
+        
       </section>
     </>
   );
