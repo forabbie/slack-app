3b280c5795065b883589c00cbf118d4227269278
workspace <workspace@void.local>
2023-06-02 01:00:35 +0800

    wip: adding of channels

--- src/components/modals/ChannelFormModal.jsx
+++ src/components/modals/ChannelFormModal.jsx
@@ -1,19 +1,20 @@
 import Modal from "./Modal";
 import Input from "../forms/Input";
 import Button from "../forms/Button";
+// import AutoSuggestInput from "../forms/AutoSuggestInput";
 import { useState } from "react";
 
 /* eslint-disable react/prop-types */
 const ChannelFormModal = (props) => {
-  const { open, auth, user, users, createChannel, fetchChannels, onClick } =
-    props;
+  const { open, auth, users, createChannel, fetchChannels, onClick } = props;
 
   const [channelName, setChannelName] = useState("");
-  const [members, setMembers] = useState("");
+  // const [members, setMembers] = useState("");
+  const [serchUser, setSerchUser] = useState("");
   const [hasError, setHasError] = useState(false);
   const [errorMessage, setErrorMessage] = useState("");
 
-  const inputClass = [
+  const inputChannelNameClass = [
     "input input-bordered w-full",
     hasError ? "input-error" : "",
   ].join(" ");
@@ -46,6 +47,10 @@ const ChannelFormModal = (props) => {
     setChannelName(value);
   };
 
+  const onSearchuUser = (value) => {
+    setSerchUser(value);
+  };
+
   // const onMemberChange = (value) => {
   //   setMember(value);
   // };
@@ -61,19 +66,19 @@ const ChannelFormModal = (props) => {
       <h3 className="font-bold text-lg text-start">Create a channel</h3>
       <div className="pt-4">
         <form onSubmit={handleSubmit}>
-          <div className="form-control w-full">
+          <div className="form-control w-full mb-4">
             <label
               htmlFor="withdraw-input"
-              className="text-md text-start normal-case mb-3"
+              className="text-md text-start normal-case mb-2"
             >
-              Name
+              Channel Name
             </label>
             <Input
               type="text"
               label={""}
               input={channelName}
-              placeholder="# channel name"
-              className={inputClass}
+              placeholder=""
+              className={inputChannelNameClass}
               onChange={onChannelNameChange}
             />
             <p className={errorMessageClass}>{errorMessage}</p>
@@ -99,6 +104,21 @@ const ChannelFormModal = (props) => {
                 </option>
               ))}
             </select> */}
+            <label
+              htmlFor="withdraw-input"
+              className="text-md text-start normal-case mb-2"
+            >
+              Add user
+            </label>
+            {/* <AutoSuggestInput users={users} /> */}
+            {/* <input
+              type="text"
+              value={serchUser}
+              name="searchuser"
+              id="serch-user"
+              className="input input-bordered w-full"
+              onChange={(e) => onSearchuUser(e.target.value)}
+            /> */}
           </div>
           <div className="modal-action">
             <Button
